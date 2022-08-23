31a73c59a9036631c91b4ed494675adb793925ef
ildar2244 <ildar2244@gmail.com>
2022-08-23 17:20:20 +0300

    Добавил либу с эмодзи.

--- src/messages/country.js
+++ src/messages/country.js
@@ -1,6 +1,8 @@
+import countryEmoji from 'country-emoji'
+
 export function formatCountryMsg(data) {
     return `
-Country: *${data.country}*
+Country: *${data.country}* ${countryEmoji.flag(data.country) || ''}
 New: *${data.cases.new}*
 Active: *${data.cases.active}*
 Recovered: *${data.cases.recovered}*
